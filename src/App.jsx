1a7daaf010e4085ffcaf9ff0939608d234687fd5
v0l0 <podolsky35alex@gmail.com>
2024-12-25 04:01:10 +0300

    1 laba

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from "react";
+
+function App() {
+  const [isBanned, setIsBanned] = useState(false);
+
+  return (
+    <div>
+      <h1>Пользователь {isBanned ? "забанен" : "не забанен"}</h1>
+      {isBanned ? (
+        <button onClick={() => setIsBanned(false)}>Разбанить</button>
+      ) : (
+        <button onClick={() => setIsBanned(true)}>Забанить</button>
+      )}
+    </div>
+  );
+}
+
+export default App;
